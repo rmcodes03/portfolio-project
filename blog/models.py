0fd463425d584b6c406819aebb62233e9b061f6a
rmcodes03 <mishrarisabh2000@gmail.com>
2023-07-10 16:42:48 +0530

    Added Blog App

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.CharField(max_length=1000)
+    blog_image = models.ImageField(upload_to='images/')
