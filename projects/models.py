da31ef5a2ab5c594634a7667545cae82195b8e75
rmcodes03 <mishrarisabh2000@gmail.com>
2023-07-14 19:53:54 +0530

    added blog page

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Projects(models.Model):
+    project_title = models.CharField(max_length=200)
+    technologies = models.CharField(max_length=200)
     project_image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.project_title
